ac7a787c08c0b519d47fe6a3681b39107c63c59a
okudahi <pabrika@mieno.jp>
2022-06-24 19:12:43 +0900

    changed to show entire time

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-
+start_time=`date +%m-%d %H:%M:%S`
 #PREFIX_SIZES=(0 80 640)
 #PREFIX_SIZES=(0 10 20 40 80 160 320 640)
 PREFIX_SIZES=(0 5 10 20 30 40 50 60 70 80)
@@ -10,5 +10,8 @@ exec_test() {
       java -Xms1g -Xmx12g Evaluate $size
     done
 }
-
+echo $start_time
 exec_test
+end_time=`date +%m-%d %H:%M:%S`
+echo $start_time
+echo $end_time
